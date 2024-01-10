6b806586e70181a32eb5aeb87bab8a1bd51a7130
suvashlucid11 <suvashneupane66@gmail.com>
2024-01-10 12:05:34 +0545

    latest assignment

--- basic.curd.mongodb.js
+++ basic.curd.mongodb.js
@@ -1,4 +1,4 @@
-use("broadway")
+// use("broadway")
 
 // db.broadway.insertOne({
 
@@ -62,4 +62,108 @@ use("broadway")
 //delete One,deleteMany
 // db.courses.deleteOne({duration:343})
 // db.courses.find();
-db.courses.deleteMany({})
+// db.courses.deleteMany({})
+// db.courses.find()
+//?update
+//? updateOne,updateMany
+// db.courses.findOne({name:"django"});
+//returned the null 
+//otherwise returns an array empty... 
+// db.courses.find()
+// db.courses.updateOne({name:"c++"},{$set:{
+//     price:50000
+// }})
+
+// db.courses.find()
+//now updateMany
+// db.courses.updateMany({name:"c++"},{
+
+//     $set:{
+//         duration:120,
+//     },
+// })
+// db.courses.find()
+//name price quantity ko assignment
+
+
+// db.sports.insertOne({
+//     sport: "football",
+//     players: 22,
+//     popular: true
+// })
+// db.sports.insertMany([
+//     {
+//         sport: "basketball",
+//         players: 10,
+//         popular: true
+//     },
+//     {
+//         sport: "tennis",
+//         players: 2,
+//         popular: true
+//     },
+//     {
+//         sport: "golf",
+//         players: 1,
+//         popular: true
+//     }
+// ])
+
+// db.sports.insertOne({
+//     name: "soccer",
+//     price: 50,
+//     quantity: 100
+// })
+
+// db.sports.insertMany([
+//     {
+//         name: "basketball",
+//         price: 40,
+//         quantity: 80
+//     },
+//     {
+//         name: "tennis",
+//         price: 30,
+//         quantity: 50
+//     },
+//     {
+//         name: "golf",
+//         price: 70,
+//         quantity: 120
+//     }
+// ])
+
+// Find all documents in the 'sports' collection
+// db.sports.find()
+
+ 
+// db.sports.find({ name: "soccer" })
+
+ 
+// db.sports.findOne({ name: "tennis" })
+
+// Delete a single document
+// db.sports.deleteOne({ name: "golf" })
+
+// Delete all documents in the 'sports' collection
+// db.sports.deleteMany({})
+// Update a single document
+// db.sports.updateOne({ name: "basketball" }, {
+//     $set: {
+//         price: 45
+//     }
+// })
+
+// Update multiple documents
+// db.sports.updateMany({ price: { $gt: 50 } }, {
+//     $inc: {
+//         quantity: 10
+//     }
+// })
+// db.sports.find()
+// db.sports.updateOne({"name":"tennis"},{$set:{
+//     "quantity":110
+// }})
+
+
+// db.movies.find()
--- closerloop.mongodb.js
+++ closerloop.mongodb.js
@@ -27,4 +27,72 @@ use("broadway")
 //logical operator
 //? $and,$or,$not,$nor 
 //status==="Ended" && runtime===60
-db.movies.find({$and:[{status:"Ended"},{runtime:60},{"rating.average":9}]})//to store multiple condition we do array
+// db.movies.find({$and:[{status:"Ended"},{runtime:60},{"rating.average":9}]})//to store multiple condition we do array
+// db.movies.find({"rating.average":{$lte:6}},{name:1,rating:1,_id:0})
+//? $or
+// db.movies.find()
+//find thse movies whose run time is less than 60 or status is Ended
+//runtime<60||status==="ended"
+// db.movies.find({$or:[{runtime:{$lt:60}},{status:"ended"}]
+
+
+// )
+//find movies whoese status is neither ended nor run time is 60
+
+// db.movies.find(
+//     {
+//         $nor: [
+//             { status: "Ended" },
+//             { runtime: 60 }
+//         ]
+//     },
+//     {
+//         name: 1,
+//         rating: 1,
+//         status: 1,
+//         runtime:1 // Exclude the _id field if you don't want it in the output
+//     }
+// )
+ 
+//find all movies 
+// db.movies.find()
+
+//find all movies 
+// db.movies.find({ status: "Ended" })
+//yesle chai end vako dekhaux  
+//tala ko chai bich ko dekhaux 
+
+// db.movies.find({
+//     $and: [
+//       { "rating.average": { $gt: 7 } },
+//       { "rating.average": { $lt: 9 } }
+//     ]
+//   })
+//tala ko shows the gereners only
+
+// db.movies.find({ genres: "Thriller" })
+//yesle shows which status is running
+
+// db.movies.find({ status: "Running" })
+
+//this code shows movies which has statues of ended and runtime 60
+// db.movies.find({
+//     $and: [
+//       { status: "Ended" },
+//       { runtime: 60 }
+//     ]
+//   })
+
+// db.movies.find({
+//     $and: [
+//       { weight: 75 },
+//       { "network.country.name": "Canada" }
+//     ]
+//   })
+//   yesle chai shows ki which has the weight of 75 and also countrybname of canada
+
+// db.movies.find({ $or: [{ weight: 96 }, { runtime: 60 }] })
+//this shows either of weight 96 or runtime of 60
+
+// db.movies.find({ "rating.average": { $ne: 9 } })
+//  this shows rating of not of 9
